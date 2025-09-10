6bf4a2fdd1d0655e29cc28520c3f3fd0010ff889
Yoon03-cloud <6631503099@lamduan.mfu.ac.th>
2025-09-10 23:52:21 +0700

    10_yoon

--- src/components/layouts/CartSheet.tsx
+++ src/components/layouts/CartSheet.tsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import { Badge } from "../../components/ui/badge";
 import { Input } from "../../components/ui/input";
 import { Label } from "../../components/ui/label";
+import { ScrollArea } from "../../components/ui/scroll-area";
+import { formatPrice } from "../../lib/utils";
+
 import {
   Sheet,
   SheetClose,
@@ -15,36 +18,78 @@ import {
 } from "../../components/ui/sheet";
 import { cartItems } from "../../data/cart";
 import { Separator } from "../../components/ui/separator";
+import { Icons } from "../icons";
+import CartItem from "../cart/CartItem";
 
-export default function SheetDemo() {
+export default function CartSheet() {
+  const itemCount = 4;
+  const amountTotal = 190;
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button variant="outline">Open</Button>
+        <Button
+          variant="outline"
+          size="icon"
+          className="relative"
+          aria-label="Open cart"
+        >
+          <Badge
+            variant="destructive"
+            className="absolute -right-2 -top-2 size-6 justify-center rounded-full p-2.5"
+          >
+            {itemCount}
+          </Badge>
+          <Icons.cart className="size-4" aria-hidden="true" />
+        </Button>
       </SheetTrigger>
-      <SheetContent>
-        <SheetHeader>
-          <SheetTitle>Edit profile</SheetTitle>
-          <SheetDescription>
-            Make changes to your profile here. Click save when you&apos;re done.
-          </SheetDescription>
+      <SheetContent className="w-full md:max-w-lg">
+        <SheetHeader className="mb-2">
+          <SheetTitle>Cart - {itemCount}</SheetTitle>
         </SheetHeader>
-        <div className="grid flex-1 auto-rows-min gap-6 px-4">
-          <div className="grid gap-3">
-            <Label htmlFor="sheet-demo-name">Name</Label>
-            <Input id="sheet-demo-name" defaultValue="Pedro Duarte" />
+        <Separator className="my-2" />
+        {cartItems.length > 0 ? (
+          <>
+            <ScrollArea className="my-4 h-[68vh] pb-8">
+              <div className="">
+                {cartItems.map((cart) => (
+                  <CartItem />
+                ))}
+              </div>
+            </ScrollArea>
+
+            <div className="space-y-4">
+              <Separator />
+              <div className="space-y-1.5 text-sm">
+                <div className="flex justify-between">
+                  <span>Shipping</span>
+                  <span>Free</span>
+                </div>
+                <div className="flex justify-between">
+                  <span>Taxes</span>
+                  <span>Calculated at checkout</span>
+                </div>
+                <div className="flex justify-between">
+                  <span>Total</span>
+                  <span>{formatPrice(amountTotal.toFixed(2))}</span>
+                </div>{" "}
+              </div>
+              <SheetFooter className="mt-4">
+                <SheetClose asChild>
+                  <Button type="submit" className="w-full" asChild>
+                    <Link to="/checkout" aria-label="check out ">
+                      Continue to checkout
+                    </Link>
+                  </Button>
+                </SheetClose>
+              </SheetFooter>
+            </div>
+          </>
+        ) : (
+          <div className="flex flex-col h-full justify-center items-center space-y-1">
+            <Icons.cart className="size-16 mb-4 text-muted-foreground" />
+            <div className="">Your cart is empty.</div>
           </div>
-          <div className="grid gap-3">
-            <Label htmlFor="sheet-demo-username">Username</Label>
-            <Input id="sheet-demo-username" defaultValue="@peduarte" />
-          </div>
-        </div>
-        <SheetFooter>
-          <Button type="submit">Save changes</Button>
-          <SheetClose asChild>
-            <Button variant="outline">Close</Button>
-          </SheetClose>
-        </SheetFooter>
+        )}
       </SheetContent>
     </Sheet>
   );
